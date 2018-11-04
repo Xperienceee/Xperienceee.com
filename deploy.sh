be411a3e763b91059106878fb819b070d253a2de
Zayn Jarvis <ZaynJarvis@gmail.com>
2018-11-04 19:47:21 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-ssh -o StrictHostKeyChecking=no root@$IP <<EOF
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/xperience root@$IP <<EOF
   echo "I was here" >> travis.txt
   cd /root/Xperienceee.com/
   ls -la
